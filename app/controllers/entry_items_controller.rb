10c239a1b6b42c7ac94f23f47aaa8daba148d159
Nicholas V Frangione <nfrangione8@gmail.com>
2021-06-03 17:30:37 -0700

    Deleted comment

--- app/controllers/entry_items_controller.rb
+++ app/controllers/entry_items_controller.rb
@@ -26,28 +26,6 @@ class EntryItemsController < ApplicationController
         render json: entry_item, include: [:user_notes]
     end
 
-    # def create
-    #     # entry_item = EntryItem.new(entry_item_params)
-    #     new_entry_item = EntryItem.new(entry_item_params)
-    #     if (EntryItem.find_by(name: new_entry_item[:name]) === false)
-    #         new_entry_item.save
-    #     end
-    #     entry_item = EntryItem.find_by(name: entry_item_params[:name])
-    #     render json: entry_item, except: [:created_at, :updated_at]
-    # end
-
-    # private
-
-    # class GetRequester
-    #     def initialize(url)
-    #         @url = url
-    #     end
-    #     def get_response_body
-    #         url = URI.parse(@url)
-    #         resp = Net::HTTP.get_response(url)
-    #         JSON.parse(resp.body)
-    #     end
-    # end
     private
     def entry_item_params
         params.require(:entry_item).permit(:category, :image_url, :name, :original_id)
@@ -66,10 +44,5 @@ class EntryItemsController < ApplicationController
         json4 = json1["data"] + json2["data"] + json3["data"]
 
         return json4
-        # json["data"].each do |entry|
-        #     if (EntryItem.find_by(name: entry["name"]) === false)
-        #         EntryItem.create(name: entry["name"], category: entry["category"], image_url: entry["image"])
-        #     end
-        # end
     end
 end
